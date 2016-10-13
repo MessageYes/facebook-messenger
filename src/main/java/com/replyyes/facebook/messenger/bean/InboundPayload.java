5005870fb78ed7fae1fbb8cd75e771a32c02736d
Chris Jordan <chris@chrisjordan.ca>
2016-10-13 14:19:45 -0700

    Javadoc tweak

--- src/main/java/com/replyyes/facebook/messenger/bean/InboundPayload.java
+++ src/main/java/com/replyyes/facebook/messenger/bean/InboundPayload.java
@@ -7,10 +7,9 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 /**
- * A {@link Callback} potentially may have a payload. This payload contains metadata specific to the
- * messenger bot. It can be set in a previous @{link OutboundMessage} as with @{link QuickReply} or app
+ * A {@link Callback} may potentially have a payload. This payload contains metadata specific to the
+ * Messenger bot. It can be set in a previous @{link OutboundMessage} as with @{link QuickReply} or app
  * configuration such as the Getting Started button or static menu.
- *
  */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
