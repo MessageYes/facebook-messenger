657506c4f5e83f3c5523b5d6b4e8ab385067283d
Chris Jordan <chris@chrisjordan.ca>
2016-10-13 16:02:05 -0700

    Javadoc tweak

--- src/main/java/com/replyyes/facebook/messenger/bean/Callback.java
+++ src/main/java/com/replyyes/facebook/messenger/bean/Callback.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 /**
- * All callbacks from the Facebook messenger platform have a common structure. This
+ * All callbacks from the Facebook Messenger platform have a common structure. This
  * class represents that structure at its highest level. It contains one or more {@link Entry}
  * instances, each of which has the potential to contain one or more {@link Messaging} instances.
  *
